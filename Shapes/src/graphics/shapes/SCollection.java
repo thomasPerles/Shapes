28b78d977a4e7cd4fd4a89c40cab72aebf61c1fe
tomtom06 <thomas.perles@uha.fr>
2016-03-29 14:11:01 +0200

    Implémentation de SCollection et SelectionAttributes
    voir commentaire sur SCollection

--- Shapes/src/graphics/shapes/SCollection.java
+++ Shapes/src/graphics/shapes/SCollection.java
@@ -22,26 +22,45 @@ public class SCollection extends Shape {
 		this.shapesCollection.add(s);
 	}
 
-	public Point getLoc() {
+	public Point getLoc() {		//renvoie le coin en haut a gauche en prenant les coordonnées min de chaque figure 
+		Point ref = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
 		for (Iterator it = iterator(); it.hasNext(); ) {
-			Point tmp = ((Shape) it.next()).getLoc(); 
+			Point tmp = ((Shape) it.next()).getLoc();
+			if (tmp.x < ref.x)
+				ref.x = tmp.x;
+			if (tmp.y < ref.y)
+				ref.y = tmp.y;
 		}
-		return null;
+		return ref;
 	}
 
-	public void setLoc(Point p) {
-		// TODO Auto-generated method stub
-		
+	public void setLoc(Point p) {		//place toutes les figures au point p
+		for (Iterator it = iterator(); it.hasNext(); )
+			((Shape) it.next()).setLoc(p);
 	}
 
 	public void translate(int x, int y) {
-		// TODO Auto-generated method stub
-		
+		for (Iterator it = iterator(); it.hasNext(); )
+			((Shape) it.next()).translate(x,y);
 	}
 
-	public Rectangle getBounds() {
-		// TODO Auto-generated method stub
-		return null;
+	public Rectangle getBounds() {		//pour chaque figure on compare les coordonnées xmin, xmax, ymin et ymax 
+		int xmin = Integer.MAX_VALUE;
+		int xmax = Integer.MIN_VALUE;
+		int ymin = Integer.MAX_VALUE;
+		int ymax = Integer.MIN_VALUE;
+		for (Iterator it = iterator(); it.hasNext(); ) {
+			Rectangle tmp = ((Shape) it.next()).getBounds();
+			if (tmp.x < xmin)
+				xmin = tmp.x;
+			if (tmp.x + tmp.width > xmax)
+				xmax = tmp.x + tmp.width;
+			if (tmp.y < ymin)
+				ymin = tmp.y;
+			if (tmp.y + tmp.height > ymax)
+				ymax = tmp.y + tmp.height;
+		}
+		return (new Rectangle(xmin, ymin, xmax - xmin, ymax - ymin));
 	}
 
 	public void accept(ShapeVisitor visitor) {
--- Shapes/src/graphics/shapes/attributes/SelectionAttributes.java
+++ Shapes/src/graphics/shapes/attributes/SelectionAttributes.java
@@ -3,9 +3,25 @@ package graphics.shapes.attributes;
 public class SelectionAttributes extends Attributes {
 
 	public static String SELECTION_ID = "selection";
+	private boolean selected;
 	
 	public String getId() {
 		return SELECTION_ID;
 	}
-
+	
+	public boolean isSelected() {
+		return selected;
+	}
+	
+	public void select() {
+		selected = true;
+	}
+	
+	public void unselect() {
+		selected = false;
+	}
+	
+	public void toggleSelection() {
+		selected = !(selected);
+	}
 }
